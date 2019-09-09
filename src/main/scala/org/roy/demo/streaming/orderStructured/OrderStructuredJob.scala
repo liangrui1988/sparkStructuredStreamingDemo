e5e2ccf13ca55c9ccb25365707488847c0d60b43
ruilaing <liangrui_1988@126.com>
2019-09-09 16:58:52 +0800

    写入到mysql

--- src/main/scala/org/roy/demo/streaming/orderStructured/OrderStructuredJob.scala
+++ src/main/scala/org/roy/demo/streaming/orderStructured/OrderStructuredJob.scala
@@ -1,10 +1,12 @@
-package org.roy.demo.streaming.bus
+package org.roy.demo.streaming.orderStructured
 
 import java.sql.Timestamp
+
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.current_timestamp
 import org.apache.spark.sql.streaming._
+import org.roy.demo.streaming.utils.JDBCSink
 import streaming.StreamingExamples
 
 /**
@@ -18,7 +20,7 @@ object StructuredStoreOrderState {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
       .builder.master("local[*]")
-      .appName("StructuredSessionization")
+      .appName("StructuredStoreOrderState")
       .getOrCreate()
     import spark.implicits._
     // Create DataFrame representing the stream of input lines from connection to host:port
@@ -27,7 +29,6 @@ object StructuredStoreOrderState {
       .option("host", "10.200.102.192")
       .option("port", 9998)
       .load().withColumn("current_timestamp", current_timestamp)
-
 
     //100,1,10,20,2019-09-03
     //1001,1,10,200,2019-09-03
@@ -39,7 +40,7 @@ object StructuredStoreOrderState {
         if (orderInfo != null && orderInfo.size > 4) {
           //分组key
           val gId = orderInfo(2) + "_" + orderInfo(1).toInt + "_" + orderInfo(4) //storeid+otype+orderdate
-          val objEvent = dataEvent(gId, orderInfo(0), orderInfo(1).toInt, orderInfo(2), orderInfo(3).toDouble, orderInfo(4), timestamp)
+          val objEvent = orderEvent(gId, orderInfo(0), orderInfo(1).toInt, orderInfo(2), orderInfo(3).toDouble, orderInfo(4), timestamp)
           objEvent
         } else {
           null
@@ -48,14 +49,14 @@ object StructuredStoreOrderState {
       }.filter(obj => obj != null)
     val orderUpdates = events
       .groupByKey(event => event.gId)
-      .mapGroupsWithState[storeOrderInfoState, storeOrderInfoStateUpdate](GroupStateTimeout.ProcessingTimeTimeout) {
+      .mapGroupsWithState[storeOrderState, storeAggUpdate](GroupStateTimeout.ProcessingTimeTimeout) {
       case (key: String, values: Iterator[dataEvent], state: GroupState[storeOrderInfoState]) =>
         val seqs = values.toSeq
         val times = seqs.map(_.timestamp).seq
         val max_time = new Timestamp(System.currentTimeMillis())
         if (state.hasTimedOut) {
           val finalUpdate =
-            storeOrderInfoStateUpdate(key, state.get.orderNum, state.get.orderMoney, max_time, expired = true)
+            storeAggUpdate(key, state.get.orderNum, state.get.orderMoney, max_time, expired = true)
           state.remove()
           finalUpdate
         } else {
@@ -75,7 +76,7 @@ object StructuredStoreOrderState {
               norderMap2 += (e.orderId -> e.money)
             })
             //取出所有的订单+流进来的订单，需要判断是否有重复订单
-            storeOrderInfoState(key, state.get.orderNum + num, state.get.orderMoney + money, stateMap ++ norderMap2, max_time)
+            storeOrderState(key, state.get.orderNum + num, state.get.orderMoney + money, stateMap ++ norderMap2, max_time)
           } else {
             var norderMap2: Map[String, Double] = Map()
             var money = 0.0
@@ -87,17 +88,16 @@ object StructuredStoreOrderState {
               }
               norderMap2 += (e.orderId -> e.money)
             })
-            storeOrderInfoState(key, norderMap2.size, money, norderMap2, max_time)
+            storeOrderState(key, norderMap2.size, money, norderMap2, max_time)
           }
           //更新缓存里面的这条数据信息
           state.update(updatedSession)
           // Set timeout such that the session will be expired if no data received for 10 seconds
           state.setTimeoutDuration("1 hour")
-          storeOrderInfoStateUpdate(key, state.get.orderNum, state.get.orderMoney, max_time, expired = false)
+          storeAggUpdate(key, state.get.orderNum, state.get.orderMoney, max_time, expired = false)
         }
     }
-    import org.apache.spark.sql.functions.udf
-    import org.apache.spark.sql.functions.lit
+    import org.apache.spark.sql.functions.{lit, udf}
 
     val storeIdCode = (gid: String, index: Int) => {
       gid.split("_")(index)
@@ -107,12 +107,12 @@ object StructuredStoreOrderState {
     import spark.implicits._
     val resultDF = orderUpdates.withColumn("storeId", storeidUDF(orderUpdates("gId"), lit(0))).
       withColumn("otype", storeidUDF(orderUpdates("gId"), lit(1))).
-      withColumn("orderDate", storeidUDF(orderUpdates("gId"), lit(2)))
+      withColumn("orderDate", storeidUDF(orderUpdates("gId"), lit(2))).drop("gId").drop("timestamp")
 
     val checkpointLocation = "/scheckpoint/checkpoint-structuredStoreOrderState"
     // Delete the checkpoint location from previous executions，socket需要删除目录
-    import java.nio.file.{Files, FileSystems}
-    import java.util.Comparator
+    import java.nio.file.{FileSystems, Files}
+
     import scala.collection.JavaConverters._
     val path = FileSystems.getDefault.getPath(checkpointLocation)
     //        .sorted(Comparator.reverseOrder())
@@ -122,27 +122,34 @@ object StructuredStoreOrderState {
         .asScala
         .foreach(p => p.toFile.delete)
     }
-
-    val query = resultDF
-      .writeStream
-      .outputMode("update")
-      .option("checkpointLocation", checkpointLocation) //user/spark/
-      .format("console")
-      .start()
+    val url = "jdbc:mysql://10.200.102.197:3306/bi"
+    val user = "root"
+    val pwd = "candao"
+    val writer = new JDBCSink(url, user, pwd)
+    val query =
+      resultDF
+        .writeStream
+        .foreach(writer)
+        .outputMode("update")
+        .trigger(Trigger.ProcessingTime(5 * 1000))
+        .start()
     query.awaitTermination()
   }
+
 }
 
 /** User-defined data type representing the input events */
-case class dataEvent(gId: String, orderId: String, otype: Int, storeId: String, money: Double, orderTime: String, timestamp: Timestamp)
-
-//最一个订单状态
-case class orderEnventInfo(orderId: String, otype: Int, storeId: String, money: Double, orderTime: String, timestamp: Timestamp)
+case class orderEvent(gId: String, orderId: String, otype: Int, storeId: String, money: Double, orderTime: String, timestamp: Timestamp)
 
 //门店里面，维护一张所有订单
-case class storeOrderInfoState(storeId: String, orderNum: Int, orderMoney: Double, orderInfoStoreMap: Map[String, Double], timestamp: Timestamp)
+case class storeOrderState(storeId: String, orderNum: Int, orderMoney: Double, orderInfoStoreMap: Map[String, Double], timestamp: Timestamp)
 
 //返回计算后的结果
-case class storeOrderInfoStateUpdate(gId: String, orderNum: Int, orderMoney: Double, timestamp: Timestamp,
-                                     expired: Boolean)
+case class storeAggUpdate(gId: String, orderNum: Int, orderMoney: Double, timestamp: Timestamp,
+                          expired: Boolean)
+
+
+
+
+
 
